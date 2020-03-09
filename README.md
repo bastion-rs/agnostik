1572c43d73dd544d929aa20bfc6c6cdb930c7e60
Justus K <justus.k@protonmail.com>
2020-03-09 18:06:40 +0100

    Add README and implement bastion layer

---
+++ README.md
@@ -0,0 +1,4 @@
+# Agnostik
+
+Agnostik is a layer between your application and the executor for your async stuff.
+It lets your switch the executors smooth and easy without having to change your applications code.
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,25 @@
 #[macro_use]
 mod macros;
 
-// ------------------------------------------
-// BASTION
-// ------------------------------------------
+use std::future::Future;
+
+pub struct Agnostik();
 
 bastion! {
-pub struct Agnostik();
+    extern crate lightproc;
+    extern crate bastion_executor;
+
+    use lightproc::{ proc_stack::ProcStack, recoverable_handle::RecoverableHandle};
+    use bastion_executor::pool;
+
+    impl Agnostik {
+        pub fn spawn<T, F>(future: F) -> RecoverableHandle<T>
+        where
+            F: Future<Output = T> + Send + 'static,
+            T: Send + 'static,
+        {
+            let proc_stack = ProcStack::default();
+            pool::spawn(future, proc_stack)
+        }
+    }
 }
