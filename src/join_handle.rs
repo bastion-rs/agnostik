65123f8a91c8f174edde323d0074c78247aa1e1b
Justus K <justus.k@protonmail.com>
2020-03-16 19:25:09 +0100

    Add RemoteHandle support

--- src/join_handle.rs
+++ src/join_handle.rs
@@ -4,10 +4,12 @@ use std::{
     task::{Context, Poll},
 };
 
-#[cfg(feature = "runtime_bastion")]
-use lightproc::recoverable_handle::RecoverableHandle;
+use futures::future::RemoteHandle;
+
 #[cfg(feature = "runtime_asyncstd")]
 use async_std::task::JoinHandle as AsyncStdHandle;
+#[cfg(feature = "runtime_bastion")]
+use lightproc::recoverable_handle::RecoverableHandle;
 
 pub struct JoinHandle<R>(InnerJoinHandle<R>);
 
@@ -16,6 +18,7 @@ pub enum InnerJoinHandle<R> {
     Bastion(RecoverableHandle<R>),
     #[cfg(feature = "runtime_asyncstd")]
     AsyncStd(AsyncStdHandle<Option<R>>),
+    RemoteHandle(RemoteHandle<R>),
 }
 
 #[cfg(feature = "runtime_bastion")]
@@ -25,23 +28,10 @@ impl<R> Future for JoinHandle<R> {
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
         match self.0 {
             #[cfg(feature = "runtime_bastion")]
-            InnerJoinHandle::Bastion(ref mut handle) => {
-                Pin::new(handle).poll(cx)
-            }
-        }
-    }
-}
-
-#[cfg(feature = "runtime_asyncstd")]
-impl<R> Future for JoinHandle<R> {
-    type Output = Option<R>;
-
-    fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
-        match self.0 {
+            InnerJoinHandle::Bastion(ref mut handle) => Pin::new(handle).poll(cx),
             #[cfg(feature = "runtime_asyncstd")]
-            InnerJoinHandle::AsyncStd(ref mut handle) => {
-                Pin::new(handle).poll(cx)
-            }
+            InnerJoinHandle::AsyncStd(ref mut handle) => Pin::new(handle).poll(cx),
+            InnerJoinHandle::RemoteHandle(ref mut handle) => Pin::new(handle).poll(cx),
         }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 #[cfg(feature = "runtime_bastion")]
 extern crate lightproc;
-
 #[cfg(feature = "runtime_asyncstd")]
 extern crate async_std;
+extern crate futures;
 
 mod join_handle;
 
