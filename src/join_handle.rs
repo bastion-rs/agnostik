fbd318c89079cd296d1e632079adf016d49ec60a
Justus K <justus.k@protonmail.com>
2020-03-16 22:23:37 +0100

    Add tokio JoinHandle support

--- src/join_handle.rs
+++ src/join_handle.rs
@@ -10,14 +10,18 @@ use futures::future::RemoteHandle;
 use async_std::task::JoinHandle as AsyncStdHandle;
 #[cfg(feature = "runtime_bastion")]
 use lightproc::recoverable_handle::RecoverableHandle;
+#[cfg(feature = "runtime_tokio")]
+use tokio::task::JoinHandle as TokioHandle;
 
 pub struct JoinHandle<R>(InnerJoinHandle<R>);
 
-pub enum InnerJoinHandle<R> {
+enum InnerJoinHandle<R> {
     #[cfg(feature = "runtime_bastion")]
     Bastion(RecoverableHandle<R>),
     #[cfg(feature = "runtime_asyncstd")]
     AsyncStd(AsyncStdHandle<Option<R>>),
+    #[cfg(feature = "runtime_tokio")]
+    Tokio(TokioHandle<R>),
     RemoteHandle(RemoteHandle<Option<R>>),
 }
 
@@ -34,6 +38,12 @@ where
             InnerJoinHandle::Bastion(ref mut handle) => Pin::new(handle).poll(cx),
             #[cfg(feature = "runtime_asyncstd")]
             InnerJoinHandle::AsyncStd(ref mut handle) => Pin::new(handle).poll(cx),
+            #[cfg(feature = "runtime_tokio")]
+            InnerJoinHandle::Tokio(ref mut handle) => match Pin::new(handle).poll(cx) {
+                // NOTE: Just panicing if Err is returned is probably not the best solution
+                Poll::Ready(val) => Poll::Ready(Some(val.expect("task failed to execute"))),
+                Poll::Pending => Poll::Pending,
+            },
             InnerJoinHandle::RemoteHandle(ref mut handle) => Pin::new(handle).poll(cx),
         }
     }
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 extern crate lightproc;
 #[cfg(feature = "runtime_asyncstd")]
 extern crate async_std;
+#[cfg(feature = "runtime_tokio")]
+extern crate tokio;
 extern crate futures;
 
 mod join_handle;
