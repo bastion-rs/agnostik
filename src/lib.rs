603eee1e409fb361c7a8513ec83d6ef0f1ab82fa
Justus K <justus.k@protonmail.com>
2020-03-18 13:47:21 +0100

    Add documentation to AgnostikExecutor methods

--- src/lib.rs
+++ src/lib.rs
@@ -12,16 +12,19 @@ use join_handle::JoinHandle;
 use std::future::Future;
 
 pub trait AgnostikExecutor {
+    /// Spawns an asynchronous task using the underlying executor.
     fn spawn<F, T>(future: F) -> JoinHandle<T>
     where
         F: Future<Output = T> + Send + 'static,
         T: Send + 'static;
 
+    /// runs the provided closure on a thread where blocking is allowed.
     fn spawn_blocking<F, T>(future: F) -> JoinHandle<T>
     where
         F: FnOnce() -> T + Send + 'static,
         T: Send + 'static;
 
+    /// Blocks until the future has finished.
     fn block_on<F, T>(future: F) -> T
     where
         F: Future<Output = T> + Send + 'static,
