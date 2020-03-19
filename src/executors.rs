f06d34dbac7d4da3a73d91aa4c1535538926122e
Justus K <justus.k@protonmail.com>
2020-03-19 01:08:00 +0100

    Add tokio AgnostikExecutor

--- src/executors.rs
+++ src/executors.rs
@@ -40,3 +40,42 @@ impl AgnostikExecutor for AsyncStdExecutor {
         async_std::task::block_on(future)
     }
 }
+
+#[cfg(feature = "runtime_tokio")]
+pub(crate) struct TokioExecutor;
+
+#[cfg(feature = "runtime_tokio")]
+impl TokioExecutor {
+    pub const fn new() -> Self {
+        TokioExecutor {}
+    }
+}
+
+#[cfg(feature = "runtime_tokio")]
+impl AgnostikExecutor for TokioExecutor {
+    fn spawn<F, T>(future: F) -> JoinHandle<T>
+    where
+        F: Future<Output = T> + Send + 'static,
+        T: Send + 'static,
+    {
+        let handle = tokio::task::spawn(future);
+        JoinHandle(InnerJoinHandle::Tokio(handle))
+    }
+
+    fn spawn_blocking<F, T>(task: F) -> JoinHandle<T>
+    where
+        F: FnOnce() -> T + Send + 'static,
+        T: Send + 'static,
+    {
+        let handle = tokio::task::spawn_blocking(task);
+        JoinHandle(InnerJoinHandle::Tokio(handle))
+    }
+
+    fn block_on<F, T>(future: F) -> T
+    where
+        F: Future<Output = T> + Send + 'static,
+        T: Send + 'static,
+    {
+        unimplemented!("Tokio's agnostik executor doesnt support running futures via this method")
+    }
+}
